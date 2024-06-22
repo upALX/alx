69642562747522e8ea3cf07076dcb0fc311aedf3
José Alexandre <j.alexandre@rock.vc>
2024-06-22 12:35:42 -0300

    adding word styles to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { mergeStyle } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const mainFont = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={mergeStyle(
+          "min-h-screen bg-background font-sans antialiased",
+          mainFont.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
