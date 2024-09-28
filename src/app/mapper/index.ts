636677b4c61429ec700a6df58599c404dea4d03a
José Alexandre <alexandreaddres@gmail.com>
2024-09-28 11:36:46 -0300

    adding mapper to projects information

--- src/app/mapper/index.ts
+++ src/app/mapper/index.ts
@@ -1,5 +1,11 @@
 import { cards_timeline } from './cards-timeline.json';
 import { faq_information } from './faq-information.json';
 import { navigation_information } from './navigation-menu-information.json';
+import { projects_information_list } from './projects-list-information.json';
 
-export { cards_timeline, faq_information, navigation_information };
+export {
+  cards_timeline,
+  faq_information,
+  navigation_information,
+  projects_information_list,
+};
--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,26 +1,12 @@
 'use client';
 
+import { projects_information_list } from '@/app/mapper';
 import CarouselPattern from '@/components/patterns/carousel-pattern';
 
 export default function ProjectsPage() {
-  const items = [
-    {
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laboriosam incidunt iure facilis enim velit perferendis, corporis soluta sunt aperiam quibusdam laborum. Laborum aperiam temporibus voluptate quis velit facilis blanditiis!',
-      mainImagePath: '/assets/weather-app.png',
-      githubPath: 'https://github.com/alexandre-bouchard/weather-app',
-      videoPath: 'https://www.youtube.com/watch?v=l9eZ3g_V72k',
-    },
-    {
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti laboriosam incidunt iure facilis enim velit perferendis, corporis soluta sunt aperiam quibusdam laborum. Laborum aperiam temporibus voluptate quis velit facilis blanditiis!',
-      mainImagePath: '/assets/weather-app.png',
-      githubPath: 'https://github.com/alexandre-bouchard/weather-app',
-      videoPath: 'https://www.youtube.com/watch?v=l9eZ3g_V72k',
-    },
-  ];
-
   return (
     <div className="grid place-items-center  min-h-screen w-full overflow-hidden">
-      <CarouselPattern items={items} />
+      <CarouselPattern items={projects_information_list} />
     </div>
   );
 }
