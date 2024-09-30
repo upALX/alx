ea7e24526d0bc55b7335673f719180779940fecb
José Alexandre <alexandreaddres@gmail.com>
2024-09-30 08:21:26 -0300

    fix lint and deploy issues

--- src/app/mapper/index.ts
+++ src/app/mapper/index.ts
@@ -1,7 +1,7 @@
-import { cards_timeline } from './cards-timeline.json';
-import { faq_information } from './faq-information.json';
-import { navigation_information } from './navigation-menu-information.json';
-import { projects_information_list } from './projects-list-information.json';
+import cards_timeline from './cards-timeline.json';
+import faq_information from './faq-information.json';
+import navigation_information from './navigation-menu-information.json';
+import projects_information_list from './projects-list-information.json';
 
 export {
   cards_timeline,
---
+++ src/app/types/about-types.ts
@@ -0,0 +1,5 @@
+export type GithubDataTypes = {
+  public_repos: number;
+  total_private_repos: number;
+  totalCommits: number;
+};
