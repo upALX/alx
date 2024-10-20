93193ae5ba71916cbec59a46844e2e2a4241e2d3
José Alexandre <alexandreaddres@gmail.com>
2024-10-20 20:06:26 -0300

    adding new statements on faq

--- src/components/patterns/accourdion-pattern.tsx
+++ src/components/patterns/accourdion-pattern.tsx
@@ -24,10 +24,37 @@ export function AccordionALX() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>What stacks am I familiar with?</AccordionTrigger>
+        <AccordionTrigger>
+          What stacks (Language & Framework) am I familiar with?
+        </AccordionTrigger>
         <AccordionContent>
-          Python with Django, Flask and Falcon. Java with Spring and Spring
-          Boot. Typescript with NodeJS, NestJS, ReactJS and NextJS.
+          Python with Django, FastAPI, Flask and Falcon. Java with Spring and
+          Spring Boot. Typescript with NodeJS, NestJS, NextJS, Fastfy, Express.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-4">
+        <AccordionTrigger>
+          What tools and libraries have I used so far?
+        </AccordionTrigger>
+        <AccordionContent>
+          React Hook Form, ZOD, TanStack, Shadcn/UI, ChakraUI, GIT, AWS (S3,
+          Lambda, ELB, Cloud Watch), GCP (Kubernetes [GKE], Cloud Build, Cloud
+          Run, Cloud Storage, Secrets, Bucket, PubSub), PostgreSQL, MySQL,
+          MongoDB, Redis, SQlite, TypeORM, DjangoORM SQLAlchemyORM, PrismaORM,
+          EloquentORM, HibernateORM, Docker, Docker Compose, TailwindCSS,
+          ReactJS, Spark, Databricks, Kafka, Pandas, Matplotlib, Grafana,
+          Prometheus, ESlint, Husky, PNPM, NPM, YARN, PIP, POETRY, Pytest.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-5">
+        <AccordionTrigger>What do I like besides technology?</AccordionTrigger>
+        <AccordionContent>
+          Hi, I&apos;m Alexandre. A fan of MMORPG style games (Lineage, New
+          World) and MOBA (League of Legends), as well as for consoles
+          (Playstation). Furthermore, i really like areas such as economics
+          (macro) and physics (cosmology and astrophysics). I like sports such
+          as bodybuilding, running and cycling, which are my main refuges, with
+          music and good headphones being my main refuges.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
